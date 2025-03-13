1fbef601fbb72dec23667bf43f17d76327b049b6
ainulkharis <ainulkharis04@gmail.com>
2025-03-13 15:37:45 +0700

    update

--- resources/views/admin/surat-masuk/index.blade.php
+++ resources/views/admin/surat-masuk/index.blade.php
@@ -12,7 +12,7 @@
                 <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item">
-                            <a href="{{ route('admin.dashboard') }}">Dashboard</a>
+                            <a href="#">Dashboard</a>
                         </li>
                         <li class="breadcrumb-item active" aria-current="page">
                             Surat Masuk
--- resources/views/user/profile/index.blade.php
+++ resources/views/user/profile/index.blade.php
@@ -14,7 +14,7 @@
                 <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item">
-                            <a href="/dashboard">Dashboard</a>
+                            <a href="#">Dashboard</a>
                         </li>
                         <li class="breadcrumb-item active" aria-current="page">
                             Profil
